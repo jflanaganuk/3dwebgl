0824e89ed555b3627e40df452f6847fb0d4c5baa
jflanaganuk <josh@wearejh.com>
2019-05-23 22:23:35 +0100

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+const app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+document.body.appendChild(app.view);
+
+const img = new PIXI.Sprite.from("image.jpg");
+img.width = window.innerWidth;
+img.height = window.innerHeight;
+app.stage.addChild(img);
+
+const depthMap = new PIXI.Sprite.from("image-map.jpg");
+app.stage.addChild(depthMap);
+
+const displacementFilter = new PIXI.filters.DisplacementFilter(depthMap);
+app.stage.filters = [displacementFilter];
+
+window.onmousemove = function(e) {
+    displacementFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 20;
+    displacementFilter.scale.y = (window.innerHeight / 2 - e.clientY) / 20;
+}
